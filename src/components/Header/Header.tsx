8df14ed0f0b21c64e6af40087a36f662da80e084
Jonatas-01 <jonatasmc02@gmail.com>
2025-10-01 02:25:54 +0100

    style: add background for navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 
 export default function Header() {
     return (
-        <header className="sticky top-0 z-50 mx-auto w-full px-6 py-3 shadow-md">
+        <header className="sticky top-0 z-50 mx-auto w-full px-6 py-3 shadow-md background-dark">
             <div className="container mx-auto max-w-6xl flex justify-between items-center flex-wrap">
                 <Logo />
                 <Nav />
--- src/components/Hero/HeroImg.tsx
+++ src/components/Hero/HeroImg.tsx
@@ -2,7 +2,7 @@
 export default function HeroImg() {
     return (
         <div className="flex justify-center">
-            <img src="../src/assets/images/heroimg.svg" alt="Hero Image" className="max-w-[34rem]" />
+            <img src="../src/assets/images/heroimg.svg" alt="Hero Image" className="max-w-[34rem] px-2" />
         </div>
     )
 }
