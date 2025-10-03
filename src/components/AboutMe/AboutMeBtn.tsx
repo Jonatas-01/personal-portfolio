930b5b1e6a71eb620c03f769136aef20611ad52b
Jonatas-01 <jonatasmc02@gmail.com>
2025-10-03 13:32:12 +0100

    style: add highlight words to about me section

--- src/components/AboutMe/AboutMeBtn.tsx
+++ src/components/AboutMe/AboutMeBtn.tsx
@@ -4,8 +4,8 @@ import { FaArrowRight } from "react-icons/fa6";
 export default function AboutMeBtn() {
     return (
         <div className="container mx-auto flex gap-6 pt-2">
-            <button className="contact-btn rounded-lg p-1 px-2 flex items-center gap-2 sm:text-lg">Check projects <FaArrowRight /></button>
-            <button className="contact-btn rounded-lg p-1 px-2 flex items-center gap-2 sm:text-lg">Contact Me <FaIdCard /></button>
+            <button className="contact-btn rounded-lg p-1 px-2 flex items-center gap-2 text-lg sm:text-xl">Check projects <FaArrowRight /></button>
+            <button className="contact-btn rounded-lg p-1 px-2 flex items-center gap-2 text-lg sm:text-xl">Contact Me <FaIdCard /></button>
         </div>
     )
 }
--- src/components/AboutMe/AboutMeTxt.tsx
+++ src/components/AboutMe/AboutMeTxt.tsx
@@ -6,7 +6,7 @@ export default function AboutMeTxt() {
                 <h2 className="text-white title text-3xl font-bold">🔥About Me</h2>
             </div>
             <div>
-                <p className="text-2xl paragraph-color">My name is Jonatas Mendes, and I’m a lifelong learner with a passion for coding. After recently completing my Full Stack Software Developer certification, I’ve been focused on mastering essential technologies and applying them in real projects. I’m now transitioning into a software development career, looking for opportunities to collaborate, grow, and contribute to building modern applications that make a difference.</p>
+                <p className="text-2xl paragraph-color">My name is Jonatas Mendes, and I'm a lifelong learner with a passion for coding. After recently completing my <span className="gradient-text">Full Stack Software Developer</span> certification, I've been focused on mastering essential technologies and applying them in <span className="gradient-text">real projects</span>. I'm now transitioning into a software development career, looking for opportunities to collaborate, grow, and contribute to building <span className="gradient-text">modern applications that make a difference</span>.</p>
             </div>
         </div>
     )
