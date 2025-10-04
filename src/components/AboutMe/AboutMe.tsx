ba3ef64803a553d982ca8dc83fd7bd2263793e5d
Jonatas-01 <jonatasmc02@gmail.com>
2025-10-04 16:58:13 +0100

    style: rescale text and image in about me section

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -5,10 +5,10 @@ import AboutMeTxt from "./AboutMeTxt";
 export default function AboutMe() {
     return (
     <div className="pt-15 container mx-auto px-4 max-w-6xl flex flex-col xl:flex-row ">
-        <div className="basis-1/2">
+        <div className="basis-4/10">
             <AboutMeImg />
         </div>
-        <div className="basis-1/2">
+        <div className="basis-6/10">
             <AboutMeTxt />
             <AboutMeBtn />
         </div>
--- src/components/AboutMe/AboutMeImg.tsx
+++ src/components/AboutMe/AboutMeImg.tsx
@@ -2,7 +2,7 @@
 export default function AboutMeImg() {
     return (
         <div className="flex justify-center">
-            <img src="../src/assets/images/profilesqr.png" alt="Profile Image" className="rounded-full h-[320px] w-[320px] xl:h-[420px] xl:w-[420px]" />
+            <img src="../src/assets/images/profilesqr.png" alt="Profile Image" className="rounded-full h-[320px] w-[320px] xl:h-[370px] xl:w-[370px]" />
         </div>
     )
 }
