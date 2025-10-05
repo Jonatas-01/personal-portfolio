5dcb46ce8c7f0107cb08cdbe88e630992aedc7ca
Jonatas-01 <jonatasmc02@gmail.com>
2025-10-05 19:23:12 +0100

    style: change gap and size of cards in skills section for big devices screen

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -3,8 +3,8 @@ import SkillsCards from "./SkillsCards";
 
 export default function Skills() {
     return (
-        <section id="skills" className="px-4">
-            <div className="mx-auto max-w-6xl pt-8">
+        <section id="skills" className="px-4 pt-14">
+            <div className="mx-auto max-w-6xl">
                 <SkillsTitle />
                 <SkillsCards />
             </div>
--- src/components/Skills/SkillsCards.tsx
+++ src/components/Skills/SkillsCards.tsx
@@ -2,10 +2,11 @@ import { FaPython, FaReact, FaCss3Alt, FaHtml5, FaGitAlt } from "react-icons/fa"
 import { SiTypescript, SiJavascript, SiPostgresql, SiPandas, SiDjango, SiTailwindcss, SiNumpy, SiHeroku } from "react-icons/si";
 import { RiNextjsFill } from "react-icons/ri";
 import SkillsTag from "./SkillsTag";
+import type { JSX } from "react/jsx-runtime";
 
 function Card({ skill, icon, level }: { skill: string; icon: JSX.Element; level: string }) {
     return (
-        <div className="skill-cards p-4 border rounded-xl flex flex-col items-center justify-center gap-2 w-[140px] h-[180px] md:w-[160px] md:h-[200px]">
+        <div className="skill-cards p-4 border rounded-xl flex flex-col items-center justify-center gap-2 w-[140px] h-[180px] md:w-[165px] md:h-[200px]">
             <span className="text-5xl">{icon}</span>
             <span className="text-xl">{skill}</span>
             <span className={`${level}-skill p-1 rounded text-white px-14`}></span>
@@ -17,7 +18,7 @@ export default function SkillsCards() {
     return (
         <div className="container mx-auto">
             <SkillsTag />
-            <div className="flex flex-wrap gap-4 justify-center md:justify-start">
+            <div className="flex flex-wrap gap-4 md:gap-6 justify-center md:justify-start">
                 <Card skill="Python" icon={<FaPython />} level="advanced" />
                 <Card skill="TypeScript" icon={<SiTypescript />} level="intermediate" />
                 <Card skill="JavaScript" icon={<SiJavascript />} level="advanced" />
@@ -26,7 +27,7 @@ export default function SkillsCards() {
                 <Card skill="CSS3" icon={<FaCss3Alt />} level="advanced" />
                 <Card skill="HTML5" icon={<FaHtml5 />} level="advanced" />
                 <Card skill="Django" icon={<SiDjango />} level="intermediate" />
-                <Card skill="Tailwind CSS" icon={<SiTailwindcss />} level="intermediate" />
+                <Card skill="Tailwind" icon={<SiTailwindcss />} level="intermediate" />
                 <Card skill="NumPy" icon={<SiNumpy />} level="intermediate" />
                 <Card skill="Heroku" icon={<SiHeroku />} level="intermediate" />
                 <Card skill="Next.js" icon={<RiNextjsFill />} level="learning" />
