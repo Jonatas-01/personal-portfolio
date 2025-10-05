02a7110246f25a3739287f85ed5220659bf12e0d
Jonatas-01 <jonatasmc02@gmail.com>
2025-10-05 19:15:21 +0100

    refact: implement function Card to prevent repetition

---
+++ src/components/Skills/SkillsTag.tsx
@@ -0,0 +1,9 @@
+
+export default function SkillsTag() {
+    return (
+        <div className="py-2 flex justify-center md:justify-start">
+            <p className="text-lg paragraph-color">Tags: <span className="advanced-skill rounded-full text-white px-3"></span>Advanced <span className="intermediate-skill rounded-full text-white px-3"></span>Intermediate <span className="learning-skill rounded-full text-white px-3"></span>Learning
+            </p>
+        </div>
+    )
+}
