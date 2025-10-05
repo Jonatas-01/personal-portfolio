0147665cd6c2caa51d7b44302393dc87ccfa3ec5
Jonatas-01 <jonatasmc02@gmail.com>
2025-10-05 19:29:43 +0100

    style: change title text in Skills section

--- src/components/Skills/SkillsTitle.tsx
+++ src/components/Skills/SkillsTitle.tsx
@@ -2,7 +2,7 @@
 export default function SkillsTitle() {
     return (
         <div className="container mx-auto">
-            <h1 className="text-white title text-3xl font-bold">⚡My Skills</h1>
+            <h1 className="text-white title text-3xl font-bold">⚡Skills</h1>
         </div>
     )
 }
