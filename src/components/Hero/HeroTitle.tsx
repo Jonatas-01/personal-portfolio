64119c54ac789f070c3804270b26b76a6bb145ad
Jonatas-01 <jonatasmc02@gmail.com>
2025-10-10 22:04:45 +0100

    feat: add dependencies for Hero text animation blur

--- src/components/Hero/HeroTitle.tsx
+++ src/components/Hero/HeroTitle.tsx
@@ -1,9 +1,22 @@
+import BlurText from "../animations/BlurText"
 
 export default function HeroTitle() {
     return (
         <div className="flex flex-col gap-4 pt-8 mx-auto px-4">
-            <h1 className="title text-4xl sm:text-6xl text-white font-semibold tracking-tight">Hello world!, I'm</h1>
-            <h1 className="title text-3xl sm:text-6xl font-semibold gradient-text tracking-tight">Jonatas Mendes</h1>
+            <BlurText
+                text="Hello world!, I'm"
+                delay={120}
+                animateBy="words"
+                direction="top"
+                className="title text-4xl sm:text-6xl text-white font-semibold tracking-tight"
+            />
+            <BlurText
+                text="Jonatas Mendes"
+                delay={150}
+                animateBy="words"
+                direction="top"
+                className="title text-3xl sm:text-6xl font-semibold secondary-color tracking-tight"
+            />
         </div>
     )
 }
