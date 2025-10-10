41beca6f151d7d86570d301790b49900ccb6783f
Jonatas-01 <jonatasmc02@gmail.com>
2025-10-10 01:55:14 +0100

    feat: add more skill cards to skill section

--- src/components/Projects/data/projectsData.ts
+++ src/components/Projects/data/projectsData.ts
@@ -16,7 +16,7 @@ export const projectsData: Projects[] = [
         imageAlt: 'DocAdvanced Project Screenshot',
         title: 'DocAdvanced',
         description: 'DocAdvanced is a hospital web app focused on prioritising user-friendly access to healthcare services. This platform enables patients to effortlessly register and request consultations with registered medical professionals. Simultaneously, it empowers doctors to manage appointment requests, confirm scheduled sessions, or cancel them when necessary.',
-        technologies: ['Python', 'Django', 'HTML5', 'CSS3', 'JavaScript', 'PostgreSQL', 'Git', 'Heroku'],
+        technologies: ['Python', 'Django', 'HTML5', 'CSS3', 'JavaScript', 'PostgreSQL', 'Git', 'Heroku', 'Tailwind'],
         documentationLink: 'https://github.com/Jonatas-01/DocAdvanced',
         liveLink: 'https://doc-advanced-c1ddb943acbd.herokuapp.com/'
     },
