40d16909239b11c1b73311e61c65c39c03669aba
Jonatas-01 <jonatasmc02@gmail.com>
2025-10-11 15:39:41 +0100

    feat: add border animaiton effect to about image

--- src/components/AboutMe/AboutMeImg.tsx
+++ src/components/AboutMe/AboutMeImg.tsx
@@ -1,8 +1,11 @@
+import profile from '@/assets/images/profilesqr.png';
 
 export default function AboutMeImg() {
     return (
         <div className="flex justify-center">
-            <img src="../src/assets/images/profilesqr.png" alt="Profile Image" className="rounded-full h-[320px] w-[320px] xl:h-[370px] xl:w-[370px]" />
+            <div className="rounded-full h-[320px] w-[320px] xl:h-[370px] xl:w-[370px] about-me-img">
+            <img src={profile} alt="Profile Image" className="rounded-full" />
+            </div>
         </div>
     )
 }
