2918034cbf7af18c56d3aa4e6b4263f45118fc96
Jonatas-01 <jonatasmc02@gmail.com>
2025-10-11 15:42:34 +0100

    feat: add target blank to anchor link in contact cards

--- src/components/Contact/ContactCards.tsx
+++ src/components/Contact/ContactCards.tsx
@@ -5,7 +5,7 @@ export default function ContactCards() {
     return (
         <div className="flex justify-center gap-6 pt-3">
             <a href="mailto:jonatasmcontact@gmail.com" className="contact-btn rounded-md p-1 px-2 flex items-center gap-2 text-lg sm:text-xl"><MdEmail /> Email Me</a>
-            <a href="https://www.linkedin.com/in/jonatas-mc/" className="contact-btn rounded-md p-1 px-2 flex items-center gap-2 text-lg sm:text-xl"><FaLinkedin /> LinkedIn</a>
+            <a href="https://www.linkedin.com/in/jonatas-mc/" target="_blank" className="contact-btn rounded-md p-1 px-2 flex items-center gap-2 text-lg sm:text-xl"><FaLinkedin /> LinkedIn</a>
         </div>
     )
 }
